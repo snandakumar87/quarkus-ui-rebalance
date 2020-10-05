51937ff268594f17ff9c5ddea441511c93413375
snandakumar87 <snandaku@redhat.com>
2020-10-05 11:09:33 -0400

    New ui for eda

--- src/main/java/org/acme/kafka/KafkaRebalancedConsumerRebalanceListener.java
+++ src/main/java/org/acme/kafka/KafkaRebalancedConsumerRebalanceListener.java
@@ -25,12 +25,6 @@ public class KafkaRebalancedConsumerRebalanceListener implements KafkaConsumerRe
 
     private static final Logger LOGGER = Logger.getLogger(KafkaRebalancedConsumerRebalanceListener.class.getName());
 
-    @ConfigProperty(name = "kafka.bootstrap.servers")
-    String bootstrap;
-
-    @ConfigProperty(name = "mp.messaging.incoming.txn.group.id")
-    String groupId;
-
     /**
      * When receiving a list of partitions will search for the earliest offset within 10 minutes
      * and seek the consumer to it.
@@ -42,50 +36,24 @@ public class KafkaRebalancedConsumerRebalanceListener implements KafkaConsumerRe
     @Override
     public Uni<Void> onPartitionsAssigned(KafkaConsumer<?, ?> consumer, Set<TopicPartition> topicPartitions) {
         long now = System.currentTimeMillis();
-        Map<String, Object> props = new HashMap();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrap);
-        //props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "us-kafka-bootstrap-kafka-us.apps.myocp.com:443");
-
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        Map<org.apache.kafka.common.TopicPartition, OffsetAndMetadata> newOffsets = null;
-
 
         long shouldStartAt = now - 60000_000L; //10 minute ago
-        try {
-           newOffsets = RemoteClusterUtils.translateOffsets(
-                    props,"operational-cluster" , groupId, Duration.ofMinutes(1));
-           System.out.println("New offsets"+newOffsets);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            e.printStackTrace();
-        }
-
-
-        Map<org.apache.kafka.common.TopicPartition, OffsetAndMetadata> finalNewOffsets = newOffsets;
         return Uni
                 .combine()
                 .all()
                 .unis(topicPartitions
                         .stream()
                         .map(topicPartition -> {
-                            for(org.apache.kafka.common.TopicPartition topicPartition1: finalNewOffsets.keySet()) {
-                                if (topicPartition1.partition() == topicPartition.getPartition())
-
-                                    LOGGER.info("Assigned " + topicPartition);
-                                    return consumer.offsetsForTimes(topicPartition, shouldStartAt)
-                                        .onItem()
-                                        .invoke(o -> LOGGER.info("Seeking to " + finalNewOffsets.get(topicPartition1).offset()))
-                                        .onItem()
-                                        .produceUni(o -> consumer
-                                                .seek(topicPartition, o == null ? 0L : finalNewOffsets.get(topicPartition1).offset())
-                                                .onItem()
-                                                .invoke(v -> LOGGER.info("Seeked to " + o))
-                                        );
-                            }
-                            return null;
+                            LOGGER.info("Assigned " + topicPartition);
+                            return consumer.committed(topicPartition)
+                                    .onItem()
+                                    .invoke(o -> LOGGER.info("Seeking to " + o))
+                                    .onItem()
+                                    .transformToUni(o -> consumer
+                                            .seek(topicPartition, o == null ? 0L : o.getOffset())
+                                            .onItem()
+                                            .invoke(v -> LOGGER.info("Seeked to " + o))
+                                    );
                         })
                         .collect(Collectors.toList()))
                 .combinedWith(a -> null);
